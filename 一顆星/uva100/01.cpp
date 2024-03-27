e4fb400f42db607f010f87a3330f28c5177c8904
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-03-27 16:45:05 +0800

    Add files via upload

---
+++ 一顆星/uva100/01.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    while (cin >> a >> b) {
+        int max_count = 0;
+        
+        for (int i = min(a, b); i <= max(a, b); i++) {
+            int num = i;
+            int count = 1;
+            
+            while (num != 1) {
+                if (num % 2 == 1) {
+                    num = 3 * num + 1;
+                } else {
+                    num = num / 2;
+                }
+                count++;
+            }
+            max_count = max(max_count, count);
+        }
+        cout << a << " " << b << " " << max_count << endl;
+    }
+
+    return 0;
+}
