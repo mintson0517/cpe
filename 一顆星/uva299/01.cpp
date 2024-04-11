7af8215f55d791465806a9330e578a64fcd00bc6
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-04-11 16:05:38 +0800

    Update 01.cpp

--- 一顆星/uva299/01.cpp
+++ 一顆星/uva299/01.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> 
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int N, L;
+    int N;
     cin >> N;
 
-    for(int j = 0; j < N; j++){
+    for (int j = 0; j < N; j++) {
+        int L;
         cin >> L;
-        
-    vector<int> numbers(L);
 
-    for (int i = 0; i < L; i++) {
-        cin >> numbers[i];
-    }
+        vector<int> numbers(L);
 
-    int swaps = 0;
-    for (int i = 0; i < L - 1; i++) {
-        for (int j = 0; j < L - i - 1; j++) {
-            if (numbers[j] > numbers[j + 1]) {
-                swap(numbers[j], numbers[j + 1]);
-                swaps++;
+        for (int i = 0; i < L; i++) {
+            cin >> numbers[i];
+        }
+
+        int swaps = 0;
+        for (int i = 0; i < L - 1; i++) {
+            for (int j = 0; j < L - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    swap(numbers[j], numbers[j + 1]);
+                    swaps++;
+                }
             }
         }
-    }
 
-    cout << "Optimal train swapping takes " << swaps << " swaps." << endl;
+        cout << "Optimal train swapping takes " << swaps << " swaps." << endl;
     }
     return 0;
 }
