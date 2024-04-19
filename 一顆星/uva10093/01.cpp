8a212a997af4bbd240e6187a820e4165cb633248
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-04-19 23:29:18 +0800

    Update 01.cpp

--- 一顆星/uva10093/01.cpp
+++ 一顆星/uva10093/01.cpp
@@ -1,67 +1,46 @@
 #include <iostream>
 #include <string>
-#include <map>
-#include <algorithm>
-
+#include <cctype>
 using namespace std;
 
-map<char, int> generate() {
-    map<char, int> num;
-    int index = 0;
+int main(){
+    string str;
+    int index, tmp, base;
+    long long int sum;
 
-    for (char i = '0'; i <= '9'; ++i) {
-        num[i] = index++;
-    }
-
-    for (char i = 'A'; i <= 'Z'; ++i) {
-        num[i] = index++;
-    }
-
-    for (char i = 'a'; i <= 'z'; ++i) {
-        num[i] = index++;
-    }
-
-    return num;
-}
-
-int solve(const string& s, const map<char, int>& num) {
-    int maxNum = 0;
-    int sumS = 0;
-
-    for (char c : s) {
-        if (num.find(c) != num.end()) {
-            maxNum = max(maxNum, num.at(c));
-            sumS += num.at(c);
+    while(getline(cin, str)){
+        sum = 0;
+        base = 1;
+        index = 0;
+        
+        while (!isalnum(str[index])){
+            index++;
         }
-    }
 
-    maxNum += 1;
-    if (maxNum <= 1) return 2;
-
-    while (maxNum <= 62) {
-        if (sumS % (maxNum - 1) == 0) {
-            return maxNum;
+        for (int i = index; i < str.length(); i++){
+            if (isdigit(str[i])){
+                tmp = str[i] - '0';
+            } else if (isupper(str[i])){
+                tmp = str[i] - 'A' + 10;
+            } else if (islower(str[i])){
+                tmp = str[i] - 'a' + 36;
+            }
+            
+            sum += tmp;
+            if (base < tmp){
+                base = tmp;
+            }
         }
-        ++maxNum;
-    }
 
-    return -1; 
-}
-
-int main() {
-    map<char, int> num = generate();
-    string s;
-
-    while (getline(cin, s)) {
-        s.erase(remove(s.begin(), s.end(), ' '), s.end());
-
-        int result = solve(s, num);
-        if (result != -1) {
-            cout << result << endl;
-        } else {
+        for (;base < 62; base++){
+            if (sum % base == 0){
+                cout << base + 1 << endl;
+                break;
+            }
+        }
+        if (base == 62){
             cout << "such number is impossible!" << endl;
         }
     }
-
     return 0;
 }
