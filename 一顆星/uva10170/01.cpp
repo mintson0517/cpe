ed880f2f56f067620929e6d76392f8cf7363d70b
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-04-19 10:12:08 +0800

    Add files via upload

---
+++ 一顆星/uva10170/01.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    long long S,D;
+    while(cin >> S >> D){
+        while(D != 0){
+            D -= S;
+            if(D <= 0){
+                cout << S << endl;
+                break;
+            }
+            else{
+                S++;
+            }
+        }
+    }
+    return 0;
+}
