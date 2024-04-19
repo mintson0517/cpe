19dbb4c4db904a61010296177af11a9aa3561207
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-04-19 10:39:17 +0800

    Add files via upload

--- 一顆星/uva11349/01.cpp
+++ 一顆星/uva11349/01.cpp
@@ -2,39 +2,38 @@
 #include <cstdio>
 using namespace std;
 
-int main() {
+int main(){
     int t; 
-    int n; 
-
+    int n;
     cin >> t;
-    for (int a = 1; a <= t; a++) {
+    for (int a = 0; a < t; a++){
         cin.ignore();
         scanf("N = %d", &n);
 
-        long long int matrix[n * n];
-        int check_neg = 1;
+        long long int matrix[n*n];
+        int check_neg = 1; 
         int check_sym = 1; 
 
-        for (int i = 0; i < n * n; i++) {
+        for (int i = 0; i < n*n; i++){
             cin >> matrix[i];
-            if (matrix[i] < 0) {
+            if (matrix[i] < 0){
                 check_neg = 0;
             }
         }
 
-        if (check_neg != 0) {
-            for (int i = 0; i < n * n / 2; i++) {
-                if (matrix[i] != matrix[n * n - i - 1]) {
+        if (check_neg != 0){
+            for (int i = 0; i < n*n/2; i++){
+                if (matrix[i] != matrix[n*n-i-1]){
                     check_sym = 0;
                     break;
                 }
             }
         }
 
-        if (check_sym && check_neg) {
-            cout << "Test #" << a << ": Symmetric." << endl;
+        if (check_sym && check_neg){
+            cout << "Test #" << a+1 << ": Symmetric." << endl;
         } else {
-            cout << "Test #" << a << ": Non-symmetric." << endl;
+            cout << "Test #" << a+1 << ": Non-symmetric." << endl;
         }
     }
     return 0;
--- 一顆星/uva11461/01.cpp
+++ 一顆星/uva11461/01.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main() {
     int a, b;
     while (cin >> a >> b) {
         if (a == 0 && b == 0) {
-            break; 
+            break;
         }
+
         int count = 0;
-        for (int i = a; i <= b; i++) {
-            int root = sqrt(i);
-            if (root * root == i) {
-                count++;
-            }
+        for (int i = sqrt(a); i * i <= b; i++) {
+            count++;
         }
-        cout << count << endl; 
+
+        cout << count << endl;
     }
     return 0;
 }
