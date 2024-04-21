5a5144b4cbee50baae1bba18290845c2d375d1e2
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-04-21 10:55:46 +0800

    Update 01.cpp

--- 一顆星/uva11417/01.cpp
+++ 一顆星/uva11417/01.cpp
@@ -1,33 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int gcd( int a, int b )
+int gcd(int a, int b)
 {
-    if( b==0 )
+    if (b == 0)
         return a;
-    return gcd( b, a%b );
+    return gcd(b, a % b);
 }
 
 int main()
 {
- int N,i,j ;
- while(cin>>N)
- {
-  if(N==0)break;
-  int G=0;
+    int N, i, j;
+    while (cin >> N)
+    {
+        if (N == 0)
+            break;
+        int G = 0;
 
-for(i=1;i<N;i++)
-{
-for(j=i+1;j<=N;j++)
+        for (i = 1; i < N; i++)
+        {
+            for (j = i + 1; j <= N; j++)
 
-{
+            {
 
-      G+=gcd(i,j);
-
-}
-
-}
-cout<<G<<endl;
- }
-return 0;
+                G += gcd(i, j);
+            }
+        }
+        cout << G << endl;
+    }
+    return 0;
 }
