77e053005de0d2ccde3638ead55e2c7c2036c67d
Mintson Huang <91959648+mintson0517@users.noreply.github.com>
2024-04-22 14:46:59 +0800

    Update 01.cpp

--- 一顆星/uva10908/01.cpp
+++ 一顆星/uva10908/01.cpp
@@ -5,38 +5,39 @@ using namespace std;
 int main(){
   int T;
   cin >> T;
-  
   for(int i = 0; i < T; i++){
     int N, M, Q, row, column;
     cin >> N >> M >> Q;
     cout << N << " " << M << " " << Q << endl;
     
     string square[N];
-    for(int i = 0; i < N; i++){
-      cin >> square[i];
+    for(int j = 0; j < N; j++){
+      cin >> square[j];
     }
+    
     
     for(int j = 0; j < Q; j++){
       cin >> row >> column;
       
-      int l_half = 0;
-      
       char center = square[row][column];
       
-      int l_height = (row < (N - row - 1)) ? row : N - row - 1;
-      int l_width = (column < (M - column - 1)) ? column : M - column - 1;
-      int half = (l_height < l_width ) ? l_height : l_width;
+      int l_half = 0;
+      int l_height = min(row, N - row - 1);
+      int l_width = min(column, M - column - 1);
+      int half = min(l_height, l_width);
       
       bool isSquare;
       for(int k = 1; k <= half; k++){
         isSquare = true;
         for(int m = row - k; m <= row + k; m++){
           for(int n = column - k; n <= column + k; n++){
-            if(square[m][n] != center){
+            if(m < 0 || m >= N || n < 0 || n >= M || square[m][n] != center){
               isSquare = false;
               break;
             }
           }
+          if(!isSquare)
+            break;
         }
         if(isSquare){
           l_half++;
